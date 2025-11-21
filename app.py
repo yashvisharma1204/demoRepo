fb7bec50f278578bb412c8dbfb1dc79f4d96f523
Yashvi Sharma <yashvi.sharma1204@gmail.com>
2025-11-21 15:44:06 +0530

    add docker test workflow

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello! Flask"
+@app.get("/")
+def index():
+    return "Docker + GitHub Actions is working!"
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
